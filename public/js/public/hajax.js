c476f037fb58cd50bc0f2dca98449fac1f9dae5a
huruji <594613537@qq.com>
2017-02-08 11:53:39 +0800

    图书管理员同意使用hajax处理请求:art:

--- public/js/public/hajax.js
+++ public/js/public/hajax.js
@@ -1,37 +1,40 @@
-function hajax(cfg) {
-    $.ajax({
-        dataType:"json",
-        async:true,
-        data:cfg.data || {},
-        url: cfg.url || '',
-        type:cfg.type || "POST",
-        beforeSend:function() {
-            cfg.beforeFn && cfg.beforeFn();
-        },
-        success:function(res){
-            cfg.successFn &&　cfg.successFn(res);
-        },
-        error:function(){
-            cfg.errorFn　&& cfg.errorFn();
-        },
-        complete:function(){
-            cfg.completeFn && cfg.completeFn();
+var hajax = {
+    ajax: function(cfg){
+        $.ajax({
+            dataType:"json",
+            async:true,
+            data:cfg.data || {},
+            url: cfg.url || '',
+            type:cfg.type || "POST",
+            beforeSend:function() {
+                cfg.beforeFn && cfg.beforeFn();
+            },
+            success:function(res){
+                cfg.successFn &&　cfg.successFn(res);
+            },
+            error:function(){
+                cfg.errorFn　&& cfg.errorFn();
+            },
+            complete:function(){
+                cfg.completeFn && cfg.completeFn();
+            }
+        });
+    },
+    hformAjax: function(cfg) {
+        var input = cfg.input || {};
+        var radio = cfg.radio || {};
+        var attr = cfg.attr || {};
+        cfg.data = cfg.data || {};
+        for(var key in input) {
+            cfg.data[key] = $.trim($('#' + input[key]).val());
         }
-    });
-}
-function hformAjax(cfg) {
-    // attr: {book:[id,attr]}
-    var input = cfg.input || {};
-    var radio = cfg.radio || {};
-    var attr = cfg.attr || {};
-    for(var key in input) {
-        cfg.data[key] = $.trim($('#' + input[key]).val());
+        for(var key in radio) {
+            cfg.data[key] = $.trim($(':radio[name=' + radio[key] + ']:checked').parent().text());
+        }
+        for(var key in attr) {
+            cfg.data[key] = $.trim($('#' + attr[key][0]).attr(attr[key][1]));
+        }
+        console.log(cfg.data);
+        this.ajax(cfg);
     }
-    for(var key in radio) {
-        cfg.data[key] = $.trim($(':radio[name=' + radio[key] + ']:checked').parent().text());
-    }
-    for(var key in attr) {
-        cfg.data[key] = $.trim($('#' + attr[key][0]).attr(attr[key][1]));
-    }
-    hajax(cfg);
-}
+};
